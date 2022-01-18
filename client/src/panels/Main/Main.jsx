5fc02095bc580e3f51106eab70232a98cd6b484f
Matvey Bouhartsev <programmer@bouhartsev.top>
2022-01-19 01:18:25 +0300

    Fix main pages

--- client/src/panels/Main/Main.jsx
+++ client/src/panels/Main/Main.jsx
@@ -19,25 +19,25 @@ import AllNotes from "./MainPanels/AllNotes/AllNotes";
 import MyNote from "./MainPanels/MyNote/MyNote";
 
 const Main = () => {
-  const [page, setPage] = useState("one");
+  const [page, setPage] = useState("all");
 
   return (
     <Panel>
-      <FixedLayout filled vertical="bottom">
+      <FixedLayout filled>
         <Epic
           activeStory={page}
           tabbar={
             <Tabbar shadow={false}>
               <TabbarItem
-                selected={page === "one"}
-                onClick={() => setPage("one")}
+                selected={page === "all"}
+                onClick={() => setPage("all")}
                 text="Новости"
               >
                 <Icon28NewsfeedOutline />
               </TabbarItem>
               <TabbarItem
-                selected={page === "two"}
-                onClick={() => setPage("two")}
+                selected={page === "my"}
+                onClick={() => setPage("my")}
                 text="Профиль"
               >
                 <Icon28UserCircleOutline />
@@ -45,8 +45,8 @@ const Main = () => {
             </Tabbar>
           }
         >
-          <AllNotes id="one" activePanel="one" />
-          <MyNote id="two" activePanel="two" />
+          <AllNotes id="all" />
+          <MyNote id="my" />
         </Epic>
       </FixedLayout>
     </Panel>
--- client/src/panels/Main/MainPanels/MyNote/MyNote.js
+++ client/src/panels/Main/MainPanels/MyNote/MyNote.js
@@ -10,12 +10,17 @@ import {
   Group,
   FixedLayout,
 } from "@vkontakte/vkui";
+import { useMutation, useQuery } from "@apollo/client";
+import { MY_NOTE } from "../../../../GraphQL/Queries";
 
 const MyNote = ({ id }) => {
+  const { loading, data } = useQuery(MY_NOTE);
+  console.log(loading, data);
+
   return (
     <View id={id} activePanel={id}>
       <Panel id={id}>
-        <Group style={{ height: "450px" }}>MyNote</Group>
+        <Group style={{ height: "150px" }}>My</Group>
       </Panel>
     </View>
   );
