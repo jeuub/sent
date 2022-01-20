29728e45495e8877c6a9f09a4b17d8d799beffd3
Matvey Bouhartsev <programmer@bouhartsev.top>
2022-01-21 01:38:38 +0300

    Starting profile page

--- client/src/panels/Main/MainPanels/MyNote/MyNote.js
+++ client/src/panels/Main/MainPanels/MyNote/MyNote.js
@@ -9,19 +9,42 @@ import {
   View,
   Group,
   FixedLayout,
+  CardGrid,
+  Card,
+  SimpleCell
 } from "@vkontakte/vkui";
 import { useMutation, useQuery } from "@apollo/client";
 import { ME } from "../../../../GraphQL/Queries";
 
 const MyNote = ({ id }) => {
-  const { loading, data } = useQuery(ME);
-  console.log(loading, data);
+  const { loading: meLoading, data: meData } = useQuery(ME);
+  console.log(meLoading, meData);
+  const username = meData?.me.username;
+  const sentence = (meData?.me.notes.length) ? meData?.me.notes[0] : null;
 
   return (
     <View id={id} activePanel={id}>
       <Panel id={id}>
-        <Group>My sent.</Group>
-        <Group>{ !loading ? data?.me.username : 'Загрузка' }</Group>
+        <PanelHeader>My sent.</PanelHeader>
+        { !meLoading ?
+        (<Group mode="plain">
+          {sentence!=null && 
+          (<CardGrid size="l">
+            <Card>
+              <blockquote>Text</blockquote>
+            </Card>
+          </CardGrid>
+          )}
+          <SimpleCell>Респекты</SimpleCell>
+          <SimpleCell>Ответы</SimpleCell>
+          <SimpleCell>Просмотры</SimpleCell>
+          <SimpleCell>Просмотры профиля</SimpleCell>
+        </Group>)
+        : <Spinner />}
+        
+        <Group>Уведомления</Group>
+        <Group>Настройки уведомлений и темы
+        </Group>
       </Panel>
     </View>
   );
